4598e243e5bfaffd9df064b25bc9a59a716cc9eb
hj9118 <hwl9118@gmail.com>
2024-07-31 18:58:57 +0900

    🗨 지명, 설명 번역

--- src/app/(home)/sidebar.tsx
+++ src/app/(home)/sidebar.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import useLocation from '@/hooks/useLocation';
+import useTranslateCity from '@/hooks/useTranslateCity';
 import Search from '../components/Search';
 import DailyWeather from '../components/DailyWeather';
 
@@ -19,12 +20,13 @@ interface WeatherData {
 const Sidebar = () => {
   const { location, error } = useLocation();
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const { translatedCity } = useTranslateCity(weatherData?.name || '');
 
   useEffect(() => {
     const fetchWeather = async () => {
       if (location.latitude && location.longitude) {
         try {
-          const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}?lat=${location.latitude}&lon=${location.longitude}&lang=kr&appid=${process.env.NEXT_PUBLIC_API_KEY}&units=metric`;
+          const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&lang=kr&appid=${process.env.NEXT_PUBLIC_API_KEY}&units=metric`;
           const response = await fetch(apiUrl);
           if (!response.ok) {
             throw new Error('Failed to fetch weather data');
@@ -48,13 +50,13 @@ const Sidebar = () => {
     return <div>Loading...</div>;
   }
 
-  const { name, main, weather } = weatherData;
+  const { main, weather } = weatherData;
 
   return (
     <aside className="flex flex-col bg-slate-200 min-h-screen px-8 py-12 gap-4 sm:w-1/3">
       <Search />
       <DailyWeather icon={weather[0].icon} />
-      <h3 className="text-xl">{name}</h3>
+      <h3 className="text-xl">{translatedCity || weatherData.name}</h3>
       <h1 className="text-6xl font-black">{Math.round(main.temp)}℃</h1>
       <h2 className="text-3xl">{weather[0].description}</h2>
     </aside>
