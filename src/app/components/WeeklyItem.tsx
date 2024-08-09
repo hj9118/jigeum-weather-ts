e660f92a10aa860916115303c316d81db3828e71
hj9118 <hwl9118@gmail.com>
2024-08-09 10:10:57 +0900

    ⚡️ cache 설정 (이후 TanstackQuery 변경)

--- src/app/components/WeeklyItem.tsx
+++ src/app/components/WeeklyItem.tsx
@@ -1,9 +1,11 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import LottieIcon from './LottieIcon';
 import { useShortDate } from '@/hooks/useFormatDate';
 import { WeeklyItemProps } from '../types';
+
+const weatherCache = new Map<string, any>();
 
 const WeeklyItem = ({
   date,
@@ -16,25 +18,61 @@ const WeeklyItem = ({
   icon,
 }: WeeklyItemProps) => {
   const [animationData, setAnimationData] = useState(null);
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
   const shortDate = useShortDate(date);
 
   useEffect(() => {
     const loadAnimation = async () => {
-      const response = await fetch(`/weatherIcon/${icon}.json`);
-      if (response.ok) {
-        const animation = await response.json();
-        setAnimationData(animation);
-      } else {
-        console.error(response.status);
+      if (isVisible) {
+        if (weatherCache.has(icon)) {
+          setAnimationData(weatherCache.get(icon));
+        } else {
+          try {
+            const response = await fetch(`/weatherIcon/${icon}.json`);
+            if (response.ok) {
+              const animation = await response.json();
+              setAnimationData(animation);
+            } else {
+              console.error(response.status);
+            }
+          } catch (error) {
+            console.error('Failed to load weatherIcon, ', error);
+          }
+        }
       }
     };
 
     loadAnimation();
-  }, [icon]);
+  }, [icon, isVisible]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      { threshold: 0.1 },
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
 
   return (
-    <div className="aspect-[3/4] bg-slate-300 max-w-96 min-w-64 rounded-xl p-4 m-2 flex flex-col">
+    <div
+      ref={ref}
+      className="aspect-[3/4] bg-slate-300 max-w-96 min-w-64 rounded-xl p-4 m-2 flex flex-col"
+    >
+      {isVisible && animationData && (
         <LottieIcon animationData={animationData} />
+      )}
       <div>
         <div>{shortDate}</div>
         <div>{description}</div>
