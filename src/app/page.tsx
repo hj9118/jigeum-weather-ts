6eae47fb3ee1fd35d7cabff1562c996f2fa355b5
jiho Heo <hwl9118@gmail.com>
2024-10-29 23:13:18 +0900

    🧪 og 태그 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,21 @@ export const metadata: Metadata = {
   title: '지금 날씨',
   description: 'Jigeum weather is...',
   manifest: '/manifest.json',
+  openGraph: {
+    title: '지금 날씨',
+    description: 'Jigeum weather is...',
+    url: 'https://jigeum-weather.vercel.app',
+    images: [
+      {
+        url: 'https://cdn.hankyung.com/photo/201905/01.19557191.1.jpg',
+        width: 800,
+        height: 600,
+        alt: '지금 날씨 이미지',
+      },
+    ],
+    locale: 'ko_KR',
+    type: 'website',
+  }
 };
 
 export default function Home() {
